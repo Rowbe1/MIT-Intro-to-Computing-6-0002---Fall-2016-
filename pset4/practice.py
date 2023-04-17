89f37271b8852d4f356f31139baedb80669bfe52
Rowbe1 <125488765+Rowbe1@users.noreply.github.com>
2023-04-17 11:14:35 +0100

    Add files via upload

---
+++ pset4/practice.py
@@ -0,0 +1,224 @@
+# Problem Set 4: Simulating the Spread of Disease and Bacteria Population Dynamics
+# Name:
+# Collaborators (Discussion):
+# Time:
+
+import math
+import numpy as np
+import pylab as pl
+import random
+
+random.seed(0)
+
+##########################
+# End helper code
+##########################
+
+class NoChildException(Exception):
+    """
+    NoChildException is raised by the reproduce() method in the SimpleBacteria
+    and ResistantBacteria classes to indicate that a bacteria cell does not
+    reproduce. You should use NoChildException as is; you do not need to
+    modify it or add any code.
+    """
+
+
+def make_one_curve_plot(x_coords, y_coords, x_label, y_label, title):
+    """
+    Makes a plot of the x coordinates and the y coordinates with the labels
+    and title provided.
+
+    Args:
+        x_coords (list of floats): x coordinates to graph
+        y_coords (list of floats): y coordinates to graph
+        x_label (str): label for the x-axis
+        y_label (str): label for the y-axis
+        title (str): title for the graph
+    """
+    pl.figure()
+    pl.plot(x_coords, y_coords)
+    pl.xlabel(x_label)
+    pl.ylabel(y_label)
+    pl.title(title)
+    pl.show()
+
+
+def make_two_curve_plot(x_coords,
+                        y_coords1,
+                        y_coords2,
+                        y_name1,
+                        y_name2,
+                        x_label,
+                        y_label,
+                        title):
+    """
+    Makes a plot with two curves on it, based on the x coordinates with each of
+    the set of y coordinates provided.
+
+    Args:
+        x_coords (list of floats): the x coordinates to graph
+        y_coords1 (list of floats): the first set of y coordinates to graph
+        y_coords2 (list of floats): the second set of y-coordinates to graph
+        y_name1 (str): name describing the first y-coordinates line
+        y_name2 (str): name describing the second y-coordinates line
+        x_label (str): label for the x-axis
+        y_label (str): label for the y-axis
+        title (str): the title of the graph
+    """
+    pl.figure()
+    pl.plot(x_coords, y_coords1, label=y_name1)
+    pl.plot(x_coords, y_coords2, label=y_name2)
+    pl.legend()
+    pl.xlabel(x_label)
+    pl.ylabel(y_label)
+    pl.title(title)
+    pl.show()
+
+
+##########################
+# PROBLEM 1
+##########################
+
+class SimpleBacteria(object):
+    """A simple bacteria cell with no antibiotic resistance"""
+
+    def __init__(self, birth_prob, death_prob):
+        """
+        Args:
+            birth_prob (float in [0, 1]): Maximum possible reproduction
+                probability
+            death_prob (float in [0, 1]): Maximum death probability
+        """
+        self.birth_prob = birth_prob
+        self.death_prob = death_prob
+
+    def is_killed(self):
+        """
+        Stochastically determines whether this bacteria cell is killed in
+        the patient's body at a time step, i.e. the bacteria cell dies with
+        some probability equal to the death probability each time step.
+
+        Returns:
+            bool: True with probability self.death_prob, False otherwise.
+        """
+        sudden_death = random.random()
+        if sudden_death <= self.death_prob:
+            return True
+        return False
+
+    def reproduce(self, pop_density):
+        """
+        Stochastically determines whether this bacteria cell reproduces at a
+        time step. Called by the update() method in the Patient and
+        TreatedPatient classes.
+
+        The bacteria cell reproduces with probability
+        self.birth_prob * (1 - pop_density).
+
+        If this bacteria cell reproduces, then reproduce() creates and returns
+        the instance of the offspring SimpleBacteria (which has the same
+        birth_prob and death_prob values as its parent).
+
+        Args:
+            pop_density (float): The population density, defined as the
+                current bacteria population divided by the maximum population
+
+        Returns:
+            SimpleBacteria: A new instance representing the offspring of
+                this bacteria cell (if the bacteria reproduces). The child
+                should have the same birth_prob and death_prob values as
+                this bacteria.
+
+        Raises:
+            NoChildException if this bacteria cell does not reproduce.
+        """
+
+        new_birth = random.random()
+        pop_birth_prob = self.birth_prob * pop_density
+
+        if new_birth <= pop_birth_prob:
+            return SimpleBacteria(self.birth_prob, self.death_prob)
+        raise NoChildException
+
+
+class Patient(object):
+    """
+    Representation of a simplified patient. The patient does not take any
+    antibiotics and his/her bacteria populations have no antibiotic resistance.
+    """
+    def __init__(self, bacteria, max_pop):
+        """
+        Args:
+            bacteria (list of SimpleBacteria): The bacteria in the population
+            max_pop (int): Maximum possible bacteria population size for
+                this patient
+        """
+        self.bacteria = bacteria
+        self.max_pop = max_pop
+
+    def get_total_pop(self):
+        """
+        Gets the size of the current total bacteria population.
+
+        Returns:
+            int: The total bacteria population
+        """
+        return len(self.bacteria)
+
+    def update(self):
+        """
+        Update the state of the bacteria population in this patient for a
+        single time step. update() should execute the following steps in
+        this order:
+
+        1. Determine whether each bacteria cell dies (according to the
+           is_killed method) and create a new list of surviving bacteria cells.
+
+        2. Calculate the current population density by dividing the surviving
+           bacteria population by the maximum population. This population
+           density value is used for the following steps until the next call
+           to update()
+
+        3. Based on the population density, determine whether each surviving
+           bacteria cell should reproduce and add offspring bacteria cells to
+           a list of bacteria in this patient. New offspring do not reproduce.
+
+        4. Reassign the patient's bacteria list to be the list of surviving
+           bacteria and new offspring bacteria
+
+        Returns:
+            int: The total bacteria population at the end of the update
+        """
+        survivors = []
+        for bac in self.bacteria:
+            if not bac.is_killed():
+                survivors.append(bac)
+        
+        pop_dens = len(survivors) / self.max_pop
+
+        for bac in survivors:
+            try:
+                survivors.append(bac.reproduce(pop_dens))
+            except NoChildException:
+                break
+        
+        self.bacteria = survivors
+        return len(self.bacteria)
+
+
+birthprob = 0.5
+deathprob = 0.3
+baccies = []
+
+for i in range(5):
+    baccies.append(SimpleBacteria(birthprob,deathprob))
+print(baccies)
+
+Pt = Patient(baccies,500)
+
+print(Pt.update())
+
+population = [[1,2,3],[4,5,6],[7,8,9]]
+print(population[1][0])
+
+print(range(0,300))
